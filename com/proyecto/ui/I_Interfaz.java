87820e9cc66ab9f7de3f8f9cf9c858261668bb0d
Jordigb44 <jordigb44@gmail.com>
2025-03-25 20:49:11 +0100

    implement I_Interfaz.java

--- com/proyecto/ui/I_Interfaz.java
+++ com/proyecto/ui/I_Interfaz.java
@@ -1,1 +1,24 @@
+package com.proyecto.ui;
 
+/**
+ * Interfaz para la gestión de la interacción con el usuario en la aplicación.
+ */
+public interface I_Interfaz {
+
+    /**
+     * Muestra la interfaz o la pantalla.
+     */
+    void mostrar();
+
+    /**
+     * Solicita una entrada del usuario.
+     * 
+     * @return La entrada del usuario.
+     */
+    String pedirEntrada();
+
+    /**
+     * Limpia la pantalla o la interfaz de usuario.
+     */
+    void limpiarPantalla();
+}
