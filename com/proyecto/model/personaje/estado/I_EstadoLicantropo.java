b22e7e3e3342d1133aeb94dc730236e7329a64dd
Jordigb44 <jordigb44@gmail.com>
2025-03-25 21:00:04 +0100

    implement I_EstadoLicantropo interface

--- com/proyecto/model/personaje/estado/I_EstadoLicantropo.java
+++ com/proyecto/model/personaje/estado/I_EstadoLicantropo.java
@@ -1,3 +1,13 @@
-public class I_EstadoLicantropo {
-    
+import java.util.List;
+
+public interface I_EstadoLicantropo {
+    // Attributes
+    int rabia = 0;
+    List<Don> dones = null;
+    float altura = 0.0f;
+    int peso = 0;
+
+    // Methods
+    void aumentarRabia(int cantidad);
+    void usarDon(Don don);
 }
