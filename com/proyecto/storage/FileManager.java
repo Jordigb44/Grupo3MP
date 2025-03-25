5773126fd8a6fcd8babbd3b1050aa9281f22a0d1
Jordigb44 <jordigb44@gmail.com>
2025-03-25 22:13:27 +0100

    create FileManager

---
+++ com/proyecto/storage/FileManager.java
@@ -0,0 +1,83 @@
+import java.util.HashMap;
+import java.util.Map;
+
+protected class FileManager {
+    private String usuarios_file;
+    private String personajes_file;
+    private String combates_file;
+    private String desafios_file;
+    private Usuario usuario;
+    private List<Personajes> personajes;
+    private List<Combates> combates;
+    private List<Desafio> desafios;
+    private List<Arma> armas;
+    private List<Armadura> armaduras;
+    private List<Notificacion> notificaciones;
+    private I_Storage storage;
+
+    private static final Map<Class<? extends I_Storage>, I_Storage> storageMap = new HashMap<>();
+
+    static {
+        // Initialize the storage map with each class and its instance
+        storageMap.put(XMLStorage.class, XMLStorage.getInstance());
+    }
+
+    protected FileManager(Class<? extends I_Storage> storageClass) {
+        this.storage = storageMap.get(storageClass); // Retrieve the appropriate storage instance
+        if (this.storage == null) {
+            throw new IllegalArgumentException("Unsupported storage class: " + storageClass);
+        }
+    }
+
+    protected String guardarUsuario(Usuario usuario) {
+        return storage.saveUsuario(usuario);
+    }
+
+    protected List<Usuario> cargarUsuarios() {
+        return storage.loadUsuarios();
+    }
+
+    protected String guardarPersonajes(List<Personajes> personajes) {
+        return storage.savePersonajes(personajes);
+    }
+
+    protected List<Personajes> cargarPersonajes() {
+        return storage.loadPersonajes();
+    }
+
+    protected String guardarCombates(List<Combates> combates) {
+        return storage.saveCombates(combates);
+    }
+
+    protected List<Combates> cargarCombates() {
+        return storage.loadCombates();
+    }
+
+    protected String guardarDesafio(Desafio desafio) {
+        return storage.saveDesafio(desafio);
+    }
+
+    protected List<Desafio> cargarDesafios() {
+        return storage.loadDesafios();
+    }
+
+    protected List<Arma> cargarArmas() {
+        return storage.loadArmas();
+    }
+
+    protected List<Armadura> cargarArmaduras() {
+        return storage.loadArmaduras();
+    }
+
+    protected Notificacion getNotificacion(Usuario usuario) {
+        return storage.getNotificacion(usuario);
+    }
+
+    protected void setNotificacion(Usuario usuario, String mensaje) {
+        storage.setNotificacion(usuario, mensaje);
+    }
+
+    protected void deleteNotificacion(Usuario usuario) {
+        storage.deleteNotificacion(usuario);
+    }
+}
