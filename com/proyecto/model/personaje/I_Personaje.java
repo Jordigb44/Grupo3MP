22398cb296d455641c63f20b12454d24d5d51514
Jordigb44 <jordigb44@gmail.com>
2025-03-25 20:56:59 +0100

    implement I_Personaje interface

--- com/proyecto/model/personaje/I_Personaje.java
+++ com/proyecto/model/personaje/I_Personaje.java
@@ -1,1 +1,29 @@
+import java.util.List;
 
+public interface Personaje {
+    // Attributes
+    String nombre = null;
+    Integer salud = null;
+    int oro = 0;
+    List<Arma> armas = null;
+    Arma armaActiva = null;
+    List<Armadura> armaduras = null;
+    Armadura armaduraActiva = null;
+    List<Esbirro> esbirros = null;
+    List<Fortaleza> fortalezas = null;
+    List<Debilidad> debilidades = null;
+
+    // Methods
+    void equiparArma(Arma arma);
+    void desequiparArma(Arma arma);
+    List<Arma> getArmas();
+    void equiparArmadura(Armadura armadura);
+    void desequiparArmadura(Armadura armadura);
+    List<Armadura> getArmaduras();
+    void atacar(Personaje objetivo);
+    void recibirDano(Integer cantidad);
+    void agregarEsbirro(Esbirro esbirro);
+    int getOro();
+    void sumarOro(int oro);
+    void restarOro(int oro);
+}
