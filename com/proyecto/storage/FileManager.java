fa93f8358a07ce5d0214cacc6a76dbbba12ff22c
Jordigb44 <jordigb44@gmail.com>
2025-03-25 22:27:50 +0100

    update filemanager

--- com/proyecto/storage/FileManager.java
+++ com/proyecto/storage/FileManager.java
@@ -1,79 +1,74 @@
+package com.proyecto.storage;
 
-    private Usuario usuario;
-    private List<Personajes> personajes;
-    private List<Combates> combates;
-    private List<Desafio> desafios;
-    private List<Arma> armas;
-    private List<Armadura> armaduras;
-    private List<Notificacion> notificaciones;
+import java.util.List;
+
+import com.proyecto.model.personaje.Personaje;
+import com.proyecto.model.personaje.habilidad.Arma;
+import com.proyecto.model.personaje.habilidad.Armadura;
+
+public class FileManager {
     private I_Storage storage;
 
-    // Map to store instances of I_Storage adapters
-    private static final Map<Class<? extends I_Storage>, I_Storage> storageMap = new HashMap<>();
-
-    static {
-        // Initialize the storage map with each adapter class and its instance
-        storageMap.put(XMLStorage.class, XMLStorage.getInstance());
-    }
-
-    // Constructor that accepts the class of the storage adapter
-    protected FileManager(Class<? extends I_Storage> storageClass) {
-        this.storage = storageMap.get(storageClass);  // Get the appropriate storage instance
-        if (this.storage == null) {
-            throw new IllegalArgumentException("Unsupported storage class: " + storageClass);
+    // Constructor that accepts a storage adapter
+    public FileManager(I_Storage storage) {
+        if (storage == null) {
+            throw new IllegalArgumentException("Storage adapter cannot be null");
+            this.storage = new storage;
         }
+        this.storage = storage;
     }
 
-    // Methods for saving and loading data
-    protected String guardarUsuario(Usuario usuario) {
-        return storage.saveUsuario(usuario);
+    // Save and load methods for different types of data, delegating the work to the storage adapter
+
+    public String guardarUsuario(Usuario usuario) {
+        return storage.guardarUsuario(usuario);
     }
 
-    protected List<Usuario> cargarUsuarios() {
-        return storage.loadUsuarios();
+    public List<Usuario> cargarUsuarios() {
+        return storage.cargarUsuarios();
     }
 
-    protected String guardarPersonajes(List<Personajes> personajes) {
-        return storage.savePersonajes(personajes);
+    public String guardarPersonajes(List<Personaje> personajes) {
+        return storage.guardarPersonajes(personajes);
     }
 
-    protected List<Personajes> cargarPersonajes() {
-        return storage.loadPersonajes();
+    public List<Personaje> cargarPersonajes() {
+        return storage.cargarPersonajes();
     }
 
-    protected String guardarCombates(List<Combates> combates) {
-        return storage.saveCombates(combates);
+    public String guardarCombates(List<Combate> combates) {
+        return storage.guardarCombates(combates);
     }
 
-    protected List<Combates> cargarCombates() {
-        return storage.loadCombates();
+    public List<Combate> cargarCombates() {
+        return storage.cargarCombates();
     }
 
-    protected String guardarDesafio(Desafio desafio) {
-        return storage.saveDesafio(desafio);
+    public String guardarDesafio(Desafio desafio) {
+        return storage.guardarDesafio(desafio);
     }
 
-    protected List<Desafio> cargarDesafios() {
-        return storage.loadDesafios();
+    public List<Desafio> cargarDesafios() {
+        return storage.cargarDesafios();
     }
 
-    protected List<Arma> cargarArmas() {
-        return storage.loadArmas();
+    public List<Arma> cargarArmas() {
+        return storage.cargarArmas();
     }
 
-    protected List<Armadura> cargarArmaduras() {
-        return storage.loadArmaduras();
+    public List<Armadura> cargarArmaduras() {
+        return storage.cargarArmaduras();
     }
 
-    protected Notificacion getNotificacion(Usuario usuario) {
+    public Notificacion getNotificacion(Usuario usuario) {
         return storage.getNotificacion(usuario);
     }
 
-    protected void setNotificacion(Usuario usuario, String mensaje) {
+    public void setNotificacion(Usuario usuario, String mensaje) {
         storage.setNotificacion(usuario, mensaje);
     }
 
-    protected void deleteNotificacion(Usuario usuario) {
+    public void deleteNotificacion(Usuario usuario) {
         storage.deleteNotificacion(usuario);
     }
 }
