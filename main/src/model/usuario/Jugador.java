548c1c2e1a6ba39aa004c5faabe14b67548a7e33
V1ctor2911 <omar29112004@gmail.com>
2025-04-06 17:12:03 +0200

    Métodos nuevos

--- main/src/model/usuario/Jugador.java
+++ main/src/model/usuario/Jugador.java
@@ -2,11 +2,13 @@ package model.usuario;
 import java.util.List;
 import model.personaje.*;
 import model.desafio.*;
+import ui.A_Interfaz; 
 
 public class Jugador {
 	private List<Personaje> personajes;
 	private Desafio desafio;
 	private int posicionRanking;
+    private A_Interfaz interfaz;
 	
 	public List<Personaje> getPersonajes() {
 		return personajes;
@@ -26,6 +28,29 @@ public class Jugador {
 	public void setPosicionRanking(int posicionRanking) {
 		this.posicionRanking = posicionRanking;
 	}
+	public Jugador getJugador() {
+	    return this;
+	}
 	
+	public void borrarPersonaje(Personaje personaje) {
+	    if (personaje != null) {
+	    	personajes.remove(personaje);
+		    interfaz.mostrar("Personaje borrado exitosamente");
+	    } else {
+	        interfaz.mostrar("No hay personajes disponibles que borrar"); ;
+	    }
+	}
+	
+	public void agregarPersonaje(Personaje personaje) {
+	    // Verificar que el personaje no sea nulo
+	    if (personaje == null) {
+	        interfaz.mostrar("Por favor introduzca, un personaje");;
+	    }  else if (personajes.contains(personaje)) {
+	        interfaz.mostrar("No puede añadir un personaje duplicado");
+	    } else {
+	    	personajes.add(personaje);
+	    } 
+	}
+
 	
 }
