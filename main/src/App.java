ca166be82c999ac1c55b76cd65a936a4a1f7d2f0
Jordigb44 <jordigb44@gmail.com>
2025-05-01 21:39:32 +0200

    improbe system close on finally

--- main/src/App.java
+++ main/src/App.java
@@ -2,15 +2,18 @@ import model.Sistema;
 
 public class App {
     public static void main(String[] args) {
+    	Sistema sistema = null;
         try {
             // Crear una instancia de Sistema que iniciará todo el flujo de la aplicación
-            new Sistema();
+            sistema = new Sistema();
             
         } catch (Exception e) {
             System.err.println("Error al iniciar el sistema: " + e.getLocalizedMessage() + e.getMessage());
         } finally {
             // Cerrar el sistema al finalizar
-            Sistema.cerrar();
+        	if (sistema != null) {
+        		sistema.cerrar();
+        	}
         }   
     }
 }
